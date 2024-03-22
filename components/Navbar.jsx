f846744f581484340f5cac67532e956474e77371
Bryan Magistrado <bmagistrado@yahoo.com>
2024-03-22 16:01:37 +0000

    Work on navbar modal

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,24 +7,35 @@ const Navbar = () => {
   const Equals = dynamic(() => import("@/components/svgComponents/EqualsSvg"));
   const Logo = dynamic(() => import("@/components/svgComponents/Logo"));
   const dynamicGap = { gap: "calc(6px + 2.25vw)" };
-  
-  return (
-    <header className="w-full px-6 py-4 text-[11px]">
-      <nav style={{...dynamicGap}} className="flex justify-between lg:justify-center items-center">
-        <Logo />
 
-        <div style={{...dynamicGap}} className="hidden md:flex justify-evenly items-center">
+  return (
+    <header className="nav z-20 w-full px-6 text-[11px] md:hover:bg-[#1d1d1f] transition-all duration-300 ease-in-out relative">
+      <nav
+        style={{ ...dynamicGap }}
+        className="flex justify-between items-center"
+      >
+        <div className="z-20">
+          <Logo />
+        </div>
+
+        <div
+          style={{ ...dynamicGap }}
+          className="hidden md:flex justify-evenly items-center"
+        >
           {navItems.map((navItem) => (
-            <div
-              className="text-nowrap cursor-pointer text-gray-200 hover:text-white transition-all duration-600 ease-in-out"
+            <span
+              className="navItem py-4 text-nowrap cursor-pointer text-gray-200 hover:text-white transition-all duration-600 ease-in-out z-[20]"
               key={navItem}
             >
               {navItem}
-            </div>
+            </span>
           ))}
         </div>
 
-        <div style={{...dynamicGap}} className="flex items-center justify-evenly">
+        <div
+          style={{ ...dynamicGap }}
+          className="flex items-center justify-center z-20"
+        >
           <Image
             className="cursor-pointer"
             src={searchImg.src}
@@ -42,9 +53,13 @@ const Navbar = () => {
           <div>
             <Equals />
           </div>
-          
         </div>
       </nav>
+      <div className="navDropdown px-6 py-10 flex justify-start items-start opacity-0 transition-all duration-[400ms] ease-in-out translate-y-[-120%] absolute left-0 w-[100vw] h-[50vh] bg-[#1d1d1f] z-[0] backdrop-blur-md">
+        <ul>List 1</ul>
+        <ul>List 2</ul>
+        <ul>List 3</ul>
+      </div>
     </header>
   );
 };
--- components/svgComponents/EqualsSvg.jsx
+++ components/svgComponents/EqualsSvg.jsx
@@ -32,7 +32,7 @@ const SvgComponent = (props) => {
       width="2.5em"
       height="2em"
       viewBox="0 0 256 256"
-      className="md:hidden fill-gray-200 transition duration-300 ease-in-out hover:fill-white cursor-pointer"
+      className="z-20 md:hidden fill-gray-200 transition duration-300 ease-in-out hover:fill-white cursor-pointer"
       {...props}
     >
       <path d="M224 160a8 8 0 0 1-8 8H40a8 8 0 0 1 0-16h176a8 8 0 0 1 8 8ZM40 104h176a8 8 0 0 0 0-16H40a8 8 0 0 0 0 16Z" />
