08f07ac3aa4041e2f4b70a7eca7ac9a962d4d467
Bryan Magistrado <bmagistrado@yahoo.com>
2024-03-29 14:57:32 +0000

    Cleaned up code in Hero, added resize event listener to change video src

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,32 @@
 "use client";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import hero from '@/public/assets/videos/hero.mp4';
 // import smallHero from '@/public/assets/videos/smallHero.mp4';
 
 const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(
-    window.innerWidth < 760
-      ? "@/public/assets/videos/smallHero.mp4"
-      : "@/public/assets/videos/hero.mp4"
+    window.innerWidth < 426
+      ? "/assets/videos/smallHero.mp4"
+      : "/assets/videos/hero.mp4"
   );
 
+  const handleVideoSrc = () => {
+    if (window.innerWidth < 426) {
+      setVideoSrc("/assets/videos/smallHero.mp4");
+    } else {
+      setVideoSrc("/assets/videos/hero.mp4");
+    }
+  };
+
   useGSAP(() => {
+    window.addEventListener('resize', handleVideoSrc)
     gsap.to(".hero-title", { opacity: 1, delay: 0.75 });
+
+    return () => {
+      window.removeEventListener('resize', handleVideoSrc)
+    }
   }, []);
 
   return (
@@ -21,15 +34,9 @@ const Hero = () => {
       <div className="h-5/6 w-full flex-center flex-col">
         <p className="hero-title">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
-          {window.innerWidth < 760 ? (
             <video autoPlay muted playsInline={true} key={videoSrc}>
-              <source src={'/assets/videos/smallHero.mp4'} type="video/mp4"/>
+              <source src={videoSrc} type="video/mp4" />
             </video>
-          ) : (
-            <video autoPlay muted playsInline={true} key={videoSrc}>
-              <source src={"/assets/videos/hero.mp4"} type="video/mp4"/>
-            </video>
-          )}
         </div>
       </div>
     </section>
