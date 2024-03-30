d526501c5acc53b293fef6ff9624038eb8abb1fc
Bryan Magistrado <bmagistrado@yahoo.com>
2024-03-30 15:24:49 +0000

    Added animation to cta

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,8 +2,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useState, useEffect } from "react";
-// import hero from '@/public/assets/videos/hero.mp4';
-// import smallHero from '@/public/assets/videos/smallHero.mp4';
 
 const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(
@@ -23,6 +21,7 @@ const Hero = () => {
   useGSAP(() => {
     window.addEventListener('resize', handleVideoSrc)
     gsap.to(".hero-title", { opacity: 1, delay: 1 });
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 1 });
 
     return () => {
       window.removeEventListener('resize', handleVideoSrc)
@@ -38,6 +37,11 @@ const Hero = () => {
               <source src={videoSrc} type="video/mp4" />
             </video>
         </div>
+      </div>
+
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From £199/month or £999</p>
       </div>
     </section>
   );
