574afed5c465e5262c9d0d6ce6f03db3906fde1e
Bryan Magistrado <bmagistrado@yahoo.com>
2024-04-01 14:39:54 +0100

    Video carousel work

--- components/Highlights.jsx
+++ components/Highlights.jsx
@@ -1,36 +1,48 @@
-"use client"
+"use client";
 import { useGSAP } from "@gsap/react";
-import gsap from 'gsap';
-import dynamic from 'next/dynamic';
+import gsap from "gsap";
+import dynamic from "next/dynamic";
 
 const Highlights = () => {
-  const VideoCarousel = dynamic(() => import('@/components/VideoCarousel'));
+  const VideoCarousel = dynamic(() => import("@/components/VideoCarousel"));
   useGSAP(() => {
-    gsap.to('#title', { opacity: 1, y: 0 });
-    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
-  })
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  });
 
   return (
-    <section id='highlights' className='w-screen overflow-hidden h-full sm:py-32 sm:px-10 common-padding bg-zinc'>
-      <div className='screen-max-w'>
-        <div className="mb-12 w-full items-center md:flex justify-between">
-          <h1 id='title' className='section-heading'>Get the highlights.</h1>
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full bg-zinc"
+    >
+      <div className="screen-max-w sm:px-10 pt-20 px-5">
+        <div className="mb-12 w-full items-center sm:flex justify-between">
+          <h1 id="title" className="section-heading tracking-tighter">
+            Get the highlights.
+          </h1>
           <div className="flex flex-wrap items-end gap-5">
             <span className="link">
               Watch the film
-              <img src={'/assets/images/watch.svg'} alt="watch" className="ml-2"/>
+              <img
+                src={"/assets/images/watch.svg"}
+                alt="watch"
+                className="ml-2"
+              />
             </span>
             <span className="link">
               Watch the event
-              <img src={'/assets/images/right.svg'} alt="watch" className="ml-2"/>
+              <img
+                src={"/assets/images/right.svg"}
+                alt="watch"
+                className="ml-2"
+              />
             </span>
           </div>
         </div>
-
-       <VideoCarousel />
       </div>
+      <VideoCarousel />
     </section>
-  )
-}
+  );
+};
 
-export default Highlights
+export default Highlights;
--- components/VideoCarousel.jsx
+++ components/VideoCarousel.jsx
@@ -4,12 +4,22 @@ import { hightlightsSlides } from '@/constants/constants'
 const VideoCarousel = () => {
   return (
     <>
-        <div className='flex items-center'>
+        <div className='flex items-center pb-32 sm:px-10'>
             {hightlightsSlides.map((list, index) => (
-                <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
+                <div key={list.id} id='slider' className='sm:pr-20'>
                     <div className='video-carousel_container'>
-                       <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+                       <div className='w-full h-full flex-center sm:rounded-3xl overflow-hidden bg-black'>
+                            <video id='video' muted loop preload='auto' playsInline={true}>
+                                <source src={list.videoSrc} type='video/mp4'/>
+                            </video>
+                       </div>
 
+                       <div className='absolute top-10 left-[10%] z-10'>
+                            {list.textLists.map((text) => (
+                                <p key={text} className='md:text-2xl text-lg font-medium tracking-tight'>
+                                    {text}
+                                </p>
+                            ))}
                        </div>
                     </div>
                 </div>
