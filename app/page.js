2bd4344aff71d6a50e3c5a75b5a9c71cb74a8b84
Bryan Magistrado <bmagistrado@yahoo.com>
2024-04-11 13:38:26 +0100

    Model title and styling changes

--- app/page.js
+++ app/page.js
@@ -4,12 +4,14 @@ const Home = () => {
   const Navbar = dynamic(() => import('@/components/Navbar'))
   const Hero = dynamic(() => import('@/components/Hero'), {ssr: false})
   const Highlights = dynamic(() => import('@/components/Highlights', {ssr: false}))
+  const Model = dynamic(() => import('@/components/Model'), {ssr: false})
 
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
--- components/Model.jsx
+++ components/Model.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+"use client"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
 
 const Model = () => {
+useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 100 })
+}, [])
+
   return (
-    <div>Model</div>
+    <section className='py-20 w-full h-full overflow-hidden'>
+        <div className='sm:px-10 px-5'>
+            <h1 id='heading' className='section-heading tracking-tighter'>
+                Take a closer look.
+            </h1>
+        </div>
+    </section>
   )
 }
 
