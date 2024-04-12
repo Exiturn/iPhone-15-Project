5481ab7ae84a16ac339027ee3634d003af5d10e3
Bryan Magistrado <bmagistrado@yahoo.com>
2024-04-12 13:04:52 +0100

    Added ModelView for small & large, added Canvas with viewport

--- components/Model.jsx
+++ components/Model.jsx
@@ -1,20 +1,22 @@
 "use client";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { useState, useRef } from "react";
 import { yellowImg } from "@/utils";
-import * as THREE from 'three';
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import * as THREE from "three";
 
 const Model = () => {
   const ModelView = dynamic(() => import("@/components/ModelView"), {
     ssr: false,
   });
 
-  const [size, setSize] = useState('small');
+  const [size, setSize] = useState("small");
   const [model, setModel] = useState({
-    title: 'iPhone 15 Pro is Natural Titanium',
-    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    title: "iPhone 15 Pro is Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
   });
 
@@ -40,7 +42,38 @@ const Model = () => {
 
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden">
-            <ModelView />
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+            >
+              <View.Port />
+            </Canvas>
           </div>
         </div>
       </div>
