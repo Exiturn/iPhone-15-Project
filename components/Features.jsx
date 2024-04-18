00b121d8749fc0aee9ee1d959907e698993c28b5
Bryan Magistrado <bmagistrado@yahoo.com>
2024-04-18 13:18:32 +0100

    Added images and scrolltrigger anim to images

--- components/Features.jsx
+++ components/Features.jsx
@@ -4,6 +4,7 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
 import { useRef } from "react";
+import { explore1Img, explore2Img } from "@/utils";
 
 const Features = () => {
   const exploreVideo = "/assets/videos/explore.mp4";
@@ -13,6 +14,15 @@ const Features = () => {
     gsap.registerPlugin(ScrollTrigger);
     const runAnimation = () => {
       animateWithGsapTrigger("#features-title", { y: 0, opacity: 1 });
+      animateWithGsapTrigger(
+        ".g_grow",
+        {
+          scale: 1,
+          opacity: 1,
+          ease: "power1",
+        },
+        { scrub: 5.5 }
+      );
     };
 
     const delay = setTimeout(() => {
@@ -26,21 +36,21 @@ const Features = () => {
 
   return (
     <section className="bg-zinc w-screen h-full py-20 sm:px-10 px-5 overflow-hidden">
-      <div className="">
+      <div className="relative ms-auto me-auto">
         <div className="mb-12">
           <h1 id="features-title" className="section-heading tracking-tighter">
             Explore the full story
           </h1>
         </div>
 
-        <div className="flex flex-col justify-center items-start overflow-hidden w-[960px] ms-auto me-auto">
-          <div className="mt-32 mb-24 flex justify-start sm:px-10">
+        <div className="flex flex-col justify-center items-start overflow-hidden lg:w-[960px] ms-auto me-auto">
+          <div className="mt-16 mb-16 flex justify-start sm:px-10">
             <h2 className="text-5xl md:text-7xl font-semibold tracking-tighter w-full">
               iPhone. <br /> Forged in titanium.
             </h2>
           </div>
 
-          <div className="flex-center flex-col sm:px-10">
+          <div className="flex-center flex-col sm:px-10 w-full">
             <div className="relative h-[50vh] w-full flex items-center">
               <video
                 playsInline
@@ -53,6 +63,25 @@ const Features = () => {
               >
                 <source src={exploreVideo} type="video/mp4" />
               </video>
+            </div>
+
+            <div className="flex flex-col w-full relative mt-4">
+              <div className="feature-video-container">
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore1Img.src}
+                    alt="titanium"
+                    className="feature-video g_grow"
+                  />
+                </div>
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore2Img.src}
+                    alt="titanium"
+                    className="feature-video g_grow"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
