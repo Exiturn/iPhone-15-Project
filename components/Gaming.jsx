f2c6c065098d8d843c6980aba14102ade459970b
Bryan Magistrado <bmagistrado@yahoo.com>
2024-04-19 13:25:59 +0100

    Added initial animation for chip

--- components/Gaming.jsx
+++ components/Gaming.jsx
@@ -1,12 +1,42 @@
+"use client";
 import { chipImg, frameImg } from "@/utils";
-import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 const Gaming = () => {
   const frameVideo = "/assets/videos/frame.mp4";
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const runAnimation = () => {
+      gsap.to(
+        "#chip",
+        { opacity: 1, scale: 1, duration: 3 },
+        {
+          scrollTrigger: {
+            trigger: "#chip",
+            start: "top 85%",
+            ease: "power1",
+            markers: true,
+          },
+        }
+      );
+    };
+
+    const delay = setTimeout(() => {
+      runAnimation();
+    }, 3000);
+
+    return () => {
+      clearTimeout(delay);
+    };
+  }, []);
+
   return (
     <section className="flex flex-col justify-center items-center overflow-hidden lg:w-[1120px] w-screen h-full py-24 sm:px-10 px-5 mx-auto">
-      <div>
-        <img src={chipImg.src} alt="graphics chip" width={180} height={180}/>
+      <div id="chip" className="my-20 opacity-0 scale-150">
+        <img src={chipImg.src} alt="graphics chip" width={180} height={180} />
       </div>
 
       <h1>
@@ -22,7 +52,7 @@ const Gaming = () => {
           </div>
           <div>
             <video>
-              <source src={frameVideo} type="video/mp4"/>
+              <source src={frameVideo} type="video/mp4" />
             </video>
           </div>
         </div>
