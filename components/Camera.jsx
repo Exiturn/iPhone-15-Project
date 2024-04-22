271fbc83489c9cac625fa22232b527e6da39e3d3
Bryan Magistrado <bmagistrado@yahoo.com>
2024-04-22 14:57:37 +0100

    Adjusted styling for camera component and added separate component for carousel

--- components/Camera.jsx
+++ components/Camera.jsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import { useEffect, useRef, useState } from "react";
 import { iguanaImg } from "@/utils";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import CameraCarousel from "./CameraCarousel";
 
 const Camera = () => {
   const scaleRef = useRef();
@@ -51,12 +52,12 @@ const Camera = () => {
   return (
     <section className="w-screen h-full py-10 md:py-24 px-5 sm:px-10 flex flex-col justify-start items-start">
       <div className="lg:w-[1120px] mx-auto">
-        <div className="w-[960px] ms-auto me-auto">
-          <h1 className="text-white w-[75%] lg:w-full mx-auto lg:mx-0 lg:text-7xl md:text-5xl text-[2.5rem] md:mb-0 mb-5 font-semibold tracking-tighter leading-none relative z-[1]">
+        <div className="w-full lg:w-[960px] ms-auto me-auto">
+          <h1 className="text-white w-full md:w-[80%] lg:w-full md:mx-auto lg:mx-0 lg:text-7xl md:text-5xl text-[2.25rem] md:mb-0 mb-5 font-semibold tracking-tighter leading-none relative z-[1]">
             A camera that captures your <br /> wildest imagination.
           </h1>
 
-          <h4 className="text-gray w-[75%] lg:w-full text-xl md:text-2xl font-semibold mt-14 relative z-[1] mx-auto lg:mx-0">
+          <h4 className="text-gray w-[75%] lg:w-full text-xl md:text-2xl font-semibold mt-14 relative z-[1] md:mx-auto lg:mx-0">
             From dramatic framing flexibility to next-generation portraits, see
             what you can do with our most powerful iPhone camera system.
           </h4>
@@ -74,14 +75,14 @@ const Camera = () => {
                 />
               </div>
             </div>
-            <div className="mt-[20vh] relative scale-[2.5] md:hidden">
+            <div className="mt-[20vh] relative scale-[1.75] md:scale-[1.25] md:hidden">
               <img
                 className="flex justify-center mt-[17px] ml-[66px]"
                 src={iguanaImg.src}
                 alt="An iguana captured on the iPhone 15's 48MP camera"
               />
             </div>
-            <p className="text-gray font-semibold relative w-[75%] lg:w-full mx-auto z-[1] mt-[15vh] md:mt-0">
+            <p className="text-gray font-semibold relative w-[75%] lg:w-full z-[1] mt-[15vh] md:mx-auto md:mt-0">
               A green iguana, captured by the 48MP Main camera
             </p>
           </div>
@@ -89,8 +90,8 @@ const Camera = () => {
       </div>
 
       <div className="mt-20 w-full">
-        <div className="w-[960px] mx-auto">
-          <p className="text-gray font-semibold relative w-[40%] z-[1] ml-[16.7%] text-xl">
+        <div className="w-full md:w-[1120px] mx-auto">
+          <p className="text-gray font-semibold relative w-full md:w-[40%] z-[1] md:ml-[16.7%] text-xl">
             With iPhone 15 Pro, you have multiple focal lengths to work with.
             It’s like having{" "}
             <span className="text-white">seven pro lenses in your pocket</span>,
@@ -98,12 +99,20 @@ const Camera = () => {
           </p>
         </div>
 
-        <div id="camera-gallery" className="w-full flex flex-col h-[30vh] relative pt-[60px]">
-          <div id="scroll-container" className="snap-x snap-mandatory w-full h-full">
-
-          </div>
-          <div className="w-[960px] mx-auto text-center">
+        <div id="camera-gallery" className="w-full flex flex-col h-[50vh] relative pt-[60px]">
+          <CameraCarousel />
+          <div className="w-full md:w-[692px] lg:w-[980px] mx-auto text-center">
             Helo man
+          </div>
+          <div id="paddle-nav" className="mt-10 place-self-end md:absolute md:right-[calc(50%-692px/2)] lg:right-[calc(50%-980px/2)] md:bottom-0">
+            <ul className="flex justify-center items-center">
+              <li className="bg-[#333336] color-white block w-[36px] h-[36px] rounded-full overflow-hidden cursor-pointer relative mr-4">
+
+              </li>
+              <li className="bg-[#333336] color-white block w-[36px] h-[36px] rounded-full overflow-hidden cursor-pointer relative">
+
+              </li>
+            </ul>
           </div>
         </div>
       </div>
---
+++ components/CameraCarousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CameraCarousel = () => {
+  return (
+    <div id="scroll-container" className="snap-x snap-mandatory w-full h-full">
+        CameraCarousel
+    </div>
+  )
+}
+
+export default CameraCarousel
